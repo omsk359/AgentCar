2f4ccf28a0f1a29d3a35fdc62413a76b8c1ed84e
Pete M <pete@studio-cube.ru>
2016-08-02 00:56:57 +0300

    agentcar.js change ip

--- public/agentcar.js
+++ public/agentcar.js
@@ -3,8 +3,8 @@
 // Localize jQuery variable
 var jQuery;
 
-//var ACurl = "localhost:3000";
-var ACurl = "146.185.180.153";
+var ACurl = "localhost:3000";
+//var ACurl = "95.85.15.40";
 
 /******** Load jQuery if not present *********/
 if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.0.0') {
