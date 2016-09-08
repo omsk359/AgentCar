6bce207976e0147a46f2cfb32533245e06d72a71
Ban Gunn <omsk359@protonmail.com>
2016-09-08 20:16:54 +0300

    userscript localhost

---
+++ public/userscript-test/agentcar-local.user.js
@@ -0,0 +1,41 @@
+﻿// ==UserScript==
+// @name           AgentCar-Local
+// @description    
+// @version        1.0
+// @include        *
+// @run-at         document-end
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_registerMenuCommand
+// ==/UserScript==
+
+console.log('AgentCar start');
+
+var id = GM_getValue('agentCarId', 'kZD2WwvnheGtest1');
+
+console.log('AgentCar id: ', id);
+
+function changeId(newId) {
+	if (id == newId) return;
+	GM_setValue('agentCarId', newId);
+	location.reload(false);
+}
+
+GM_registerMenuCommand('Lada', changeId.bind(null, 'kZD2WwvnheGtest1'), 'L');
+GM_registerMenuCommand('Skoda', changeId.bind(null, 'kZD2WwvnheGtest2'), 'S');
+
+
+var script = document.createElement('script');
+
+script.src = '//localhost:3000/agentcar.js';
+script.charset = 'UTF-8';
+script.type = 'text/javascript';
+script.setAttribute('data-id', id);
+
+document.body.appendChild(script);
+
+console.log('AgentCar added');
+
+
+if (typeof localStorage != 'undefined')
+	localStorage.setItem('agentCarDebug', 1);
