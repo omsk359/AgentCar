bf0f390cbfd840badf92508d3dee701f3bfd0b4b
Ban Gunn <omsk359@protonmail.com>
2016-09-09 13:05:02 +0300

    skoda cars

---
+++ imports/startup/server/skoda_json.js
@@ -0,0 +1,429 @@
+export default [
+	{
+		"photo": "https://4.downloader.disk.yandex.ru/preview/ce0c6e60415b524d36813693e147efa9bbdbf0fe0d1b88f0156df218788ccc83/inf/PuAlW5aejdHA25NE0Ie--YGAcB8fZxSWfbwYilvrJGvrnG7ZsXBiPF0qBin88umI_OzepKrQTljhTeGw4NO5uw%3D%3D?uid=2648717&filename=Yeti1.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+		"mark": "SKODA",
+		"model": "Yeti",
+		"equipment": "Outdoor Active",
+		"carcase": "",
+		"color": "Бежевый Cappuccino металлик",
+		"mileage": 0,
+		"year": 2016,
+		"engine": {
+			"type": "бензиновый",
+			"capacity": "1,6",
+			"power": "110 л.с."
+		},
+		"kpp": "АКПП",
+		"priceold": 1158500,
+		"price": 1070615
+	}, {
+	"photo": "https://4.downloader.disk.yandex.ru/preview/ce0c6e60415b524d36813693e147efa9bbdbf0fe0d1b88f0156df218788ccc83/inf/PuAlW5aejdHA25NE0Ie--YGAcB8fZxSWfbwYilvrJGvrnG7ZsXBiPF0qBin88umI_OzepKrQTljhTeGw4NO5uw%3D%3D?uid=2648717&filename=Yeti1.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Yeti",
+	"equipment": "Outdoor Ambition",
+	"carcase": "",
+	"color": "Бежевый Cappuccino металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,4",
+		"power": "125 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 1308900,
+	"price": 1173463
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/1835ad58071d5f0c3a2dcc23eeb44058a29545172b094a95014cead3a5ea93cb/inf/PuAlW5aejdHA25NE0Ie--WtnR3c0U66eNsEpgKW4g4kJv7EHEZ5tyHv2j4kkKZzlN3NbZnosEcWkqnU8qNCbFw%3D%3D?uid=2648717&filename=Yeti6.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Yeti",
+	"equipment": "Outdoor Ambition",
+	"carcase": "",
+	"color": "Серый Platinum металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,8",
+		"power": "152 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 14887000,
+	"price": 1361515
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/7f7069a0b89dc3d037eaba981f8ee341d255dbe91ab85e658a8f8b0534dda215/inf/PuAlW5aejdHA25NE0Ie--QYey6J__4d7ePh-QXFn3VT16pMC9L2yaI5Sqe-jl0pjDodPw6wHEyqbmZIp3sVkFQ%3D%3D?uid=2648717&filename=Yeti5.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Yeti",
+	"equipment": "Outdoor Active",
+	"carcase": "",
+	"color": "Белый Pure",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 1140800,
+	"price": 1052915
+},{
+	"photo": "https://2.downloader.disk.yandex.ru/preview/00fe25c14ee4723c0a5a14bb65f16e6b267dbf643146756fadf80104e83c418d/inf/PuAlW5aejdHA25NE0Ie--au_xaK9XFrJzv74arhGV2b3ZGXpH97GTWSrUcczUj3HiD1cqE3qoamZ6B1QTOlWDA%3D%3D?uid=2648717&filename=Yeti7.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Yeti",
+	"equipment": "Active",
+	"carcase": "",
+	"color": "Серебристый Brilliant металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 1094800,
+	"price": 1006915
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/3e003ced99ab320025dc3a1c3c6af63d482a0fbd1afae8c5947faedf9e6690b8/inf/PuAlW5aejdHA25NE0Ie--WGlK0wgRMzhgKX9pRRzfNzY2El0FsA9KHSd1tPkwvFJB3MnxPQ4c9UtkspVVQipEQ%3D%3D?uid=2648717&filename=Yeti3.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Yeti",
+	"equipment": "Outdoor Ambition",
+	"carcase": "",
+	"color": "Синий Storm металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 1308700,
+	"price": 1173563
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/7f7069a0b89dc3d037eaba981f8ee341d255dbe91ab85e658a8f8b0534dda215/inf/PuAlW5aejdHA25NE0Ie--QYey6J__4d7ePh-QXFn3VT16pMC9L2yaI5Sqe-jl0pjDodPw6wHEyqbmZIp3sVkFQ%3D%3D?uid=2648717&filename=Yeti5.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Yeti",
+	"equipment": "Outdoor Ambition",
+	"carcase": "",
+	"color": "Белый Pure с черной крышей",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,4",
+		"power": "125 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 1343300,
+	"price": 1208163
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/fe29525e36ca6101dddc9e44757fbe14bcccb7b6a56c87a4e0a0531d11ec907c/inf/PuAlW5aejdHA25NE0Ie--SmDF6Gn1MUh4ADyIPrO7lnnGdlPCSWFbcwvL7rs8pJStFKknTswQhwB9cKRF4tSIg%3D%3D?uid=2648717&filename=Rapid1%203.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Style",
+	"carcase": "",
+	"color": "Черный Deep перламутр",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 909600,
+	"price": 861380
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/fe29525e36ca6101dddc9e44757fbe14bcccb7b6a56c87a4e0a0531d11ec907c/inf/PuAlW5aejdHA25NE0Ie--SmDF6Gn1MUh4ADyIPrO7lnnGdlPCSWFbcwvL7rs8pJStFKknTswQhwB9cKRF4tSIg%3D%3D?uid=2648717&filename=Rapid1%203.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Красный Wild Cherry металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,4",
+		"power": "125 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 957900,
+	"price": 904742
+},{
+	"photo": "https://2.downloader.disk.yandex.ru/preview/48d38085de0c461fdf2bab5dcca2bef505d0b2e8af2ef23f518206a3b9605f08/inf/PuAlW5aejdHA25NE0Ie--e0_oSjTdiAC-EhVYUUAC82foXXdJbLaAg7dt3AlueRQ6uVQZWkBPrUpyugenXdVhQ%3D%3D?uid=2648717&filename=Rapid%2010.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Синий Denim металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 917900,
+	"price": 868887
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/c6c5c81358c3cad56ed9fa55ba8d150d04636bd8e96dc51fdb54e260f7e55e16/inf/PuAlW5aejdHA25NE0Ie--bafWr47hAotbCdq69DrbTgbeGmIF-ckdXCASgWTAvzEhv3jUtnajTUBb5b_Zl6_Nw%3D%3D?uid=2648717&filename=Rapid%206.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Active",
+	"carcase": "",
+	"color": "Белый Pure",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 731200,
+	"price": 720201
+},{
+	"photo": "https://2.downloader.disk.yandex.ru/preview/bc3634be97e59c954101964b739cc20e4202732f2cf6f17339898c83d2e20425/inf/PuAlW5aejdHA25NE0Ie--R6mkwbHoggGh1h3FNDbtEh6jztYGh7QrWaYUVAwQ2nhql7I7ZmtPkc2lw1TqdxyHg%3D%3D?uid=2648717&filename=Rapid%201.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Style",
+	"carcase": "",
+	"color": "Белый Pure с черной крышей",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,4",
+		"power": "125 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 1032700,
+	"price": 985000
+},{
+	"photo": "https://2.downloader.disk.yandex.ru/preview/bc3634be97e59c954101964b739cc20e4202732f2cf6f17339898c83d2e20425/inf/PuAlW5aejdHA25NE0Ie--R6mkwbHoggGh1h3FNDbtEh6jztYGh7QrWaYUVAwQ2nhql7I7ZmtPkc2lw1TqdxyHg%3D%3D?uid=2648717&filename=Rapid%201.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Белый Pure с черной крышей",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,4",
+		"power": "125 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 987800,
+	"price": 961791
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/fe29525e36ca6101dddc9e44757fbe14bcccb7b6a56c87a4e0a0531d11ec907c/inf/PuAlW5aejdHA25NE0Ie--SmDF6Gn1MUh4ADyIPrO7lnnGdlPCSWFbcwvL7rs8pJStFKknTswQhwB9cKRF4tSIg%3D%3D?uid=2648717&filename=Rapid1%203.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Красный Wild Cherry металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 900300,
+	"price": 858925
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/fe29525e36ca6101dddc9e44757fbe14bcccb7b6a56c87a4e0a0531d11ec907c/inf/PuAlW5aejdHA25NE0Ie--SmDF6Gn1MUh4ADyIPrO7lnnGdlPCSWFbcwvL7rs8pJStFKknTswQhwB9cKRF4tSIg%3D%3D?uid=2648717&filename=Rapid1%203.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Rapid",
+	"equipment": "Active",
+	"carcase": "",
+	"color": "Серебристый Brilliant металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 816100,
+	"price": 785795
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/805f068f1023dbbc1e4818fafe8bae555f68fe87e9319662b3115328b5767625/inf/PuAlW5aejdHA25NE0Ie--YoN98zuYKwjoYBz1o7qrx32afVjqZWq4PIFBIqDotftS5XGpWCxT6AhdAIiGqH8mw%3D%3D?uid=2648717&filename=Octavia%20Sedan%207.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Style",
+	"carcase": "",
+	"color": "Белый Pure",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 1263000,
+	"price": 1204900
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/00247e8dde4ff3e52276806484da86948d3448d5de361929bfdb1e290ecbacdf/inf/PuAlW5aejdHA25NE0Ie--TJHGoXmHXDYRfQAyy_QysC_0pYXQAVzxtKdHOI2ahAeilpMKAZjKXi9qpP_gnPS-w%3D%3D?uid=2648717&filename=Octavia%20Sedan%201.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Черный Deep перламутр",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 1109300,
+	"price": 1085548
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/7480fdc80cf08b2e2526b2758beb01f3552e012187211526f0f8f4d68f673368/inf/PuAlW5aejdHA25NE0Ie--egUw8vRWR3IcwZ7zYX_OBrTvWcyoKdRr993m1u_r_o80V5hHS8sRdu2qrZX7XtWOQ%3D%3D?uid=2648717&filename=Octavia%20Sedan%204.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Синий Storm металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,8",
+		"power": "180 л.с."
+	},
+	"kpp": "DSG",
+	"priceold":  1306300,
+	"price": 1257150
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/8ffe99c172fd7ca54ebc09d9d009da42e904f4ed70bae983108e1b30a9dc53fb/inf/PuAlW5aejdHA25NE0Ie--Vw5l-_u5kCqEZG9WwZBOunDwOGXbyLHzika21qOM8e0Zt6-Nu9l1gcrX7swLFut9A%3D%3D?uid=2648717&filename=Octavia%20Sedan%205.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Бежевый Cappuccino металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 1195900,
+	"price": 1170250
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/b6e3b11c229a2c064de4ec9e9ca567856b27e099be6c6359f286a321826edbee/inf/PuAlW5aejdHA25NE0Ie--RNgO2fGvWgYOJE3-wfJoF19RBx7FRmCs3lfvCmIy4Foeh7VmAUiMYY0EtUYQFlz5w%3D%3D?uid=2648717&filename=Octavia%20Sedan%209.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Серый Platinum металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,8",
+		"power": "180 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 1313000,
+	"price": 1287350
+},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/8ffe99c172fd7ca54ebc09d9d009da42e904f4ed70bae983108e1b30a9dc53fb/inf/PuAlW5aejdHA25NE0Ie--Vw5l-_u5kCqEZG9WwZBOunDwOGXbyLHzika21qOM8e0Zt6-Nu9l1gcrX7swLFut9A%3D%3D?uid=2648717&filename=Octavia%20Sedan%205.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Бежевый Cappuccino металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 1186500,
+	"price": 1160850
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/805f068f1023dbbc1e4818fafe8bae555f68fe87e9319662b3115328b5767625/inf/PuAlW5aejdHA25NE0Ie--YoN98zuYKwjoYBz1o7qrx32afVjqZWq4PIFBIqDotftS5XGpWCxT6AhdAIiGqH8mw%3D%3D?uid=2648717&filename=Octavia%20Sedan%207.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Active",
+	"carcase": "",
+	"color": "Белый Pure",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "МКПП",
+	"priceold": 920000,
+	"price": 860000
+},{
+	"photo": "https://3.downloader.disk.yandex.ru/preview/805f068f1023dbbc1e4818fafe8bae555f68fe87e9319662b3115328b5767625/inf/PuAlW5aejdHA25NE0Ie--YoN98zuYKwjoYBz1o7qrx32afVjqZWq4PIFBIqDotftS5XGpWCxT6AhdAIiGqH8mw%3D%3D?uid=2648717&filename=Octavia%20Sedan%207.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Белый Pure",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,8",
+		"power": "180 л.с."
+	},
+	"kpp": "DSG",
+	"priceold": 1295900,
+	"price": 1270250
+},
+	{
+		"photo": "https://3.downloader.disk.yandex.ru/preview/805f068f1023dbbc1e4818fafe8bae555f68fe87e9319662b3115328b5767625/inf/PuAlW5aejdHA25NE0Ie--YoN98zuYKwjoYBz1o7qrx32afVjqZWq4PIFBIqDotftS5XGpWCxT6AhdAIiGqH8mw%3D%3D?uid=2648717&filename=Octavia%20Sedan%207.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+		"mark": "SKODA",
+		"model": "Octavia",
+		"equipment": "Style",
+		"carcase": "",
+		"color": "Белый Pure с черной крышей",
+		"mileage": 0,
+		"year": 2016,
+		"engine": {
+			"type": "бензиновый",
+			"capacity": "1,8",
+			"power": "180 л.с."
+		},
+		"kpp": "DSG",
+		"priceold": 1290700,
+		"price": 1230700
+	},{
+	"photo": "https://1.downloader.disk.yandex.ru/preview/b6e3b11c229a2c064de4ec9e9ca567856b27e099be6c6359f286a321826edbee/inf/PuAlW5aejdHA25NE0Ie--RNgO2fGvWgYOJE3-wfJoF19RBx7FRmCs3lfvCmIy4Foeh7VmAUiMYY0EtUYQFlz5w%3D%3D?uid=2648717&filename=Octavia%20Sedan%209.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&tknv=v2&size=222x222",
+	"mark": "SKODA",
+	"model": "Octavia",
+	"equipment": "Ambition",
+	"carcase": "",
+	"color": "Серый Platinum металлик",
+	"mileage": 0,
+	"year": 2016,
+	"engine": {
+		"type": "бензиновый",
+		"capacity": "1,6",
+		"power": "110 л.с."
+	},
+	"kpp": "АКПП",
+	"priceold": 1206100,
+	"price": 1180450
+},
+]
