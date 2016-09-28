678b92b6755a9890e65b900a354cd9391b75a68e
ubuntu <aa@bb.aa>
2016-09-28 09:24:34 +0300

    game text

--- imports/client/game/helpers.js
+++ imports/client/game/helpers.js
@@ -1,4 +1,4 @@
-let resultTypes = ["дополнительное оборудование на выбор", "скидка на КАСКО 15%", "бесплатное ТО3"];
+let resultTypes = ["скидка на дополнительное оборудование 15%", "скидка на КАСКО 15%", "бесплатное ТО"];
 
 export function getResult(type) {
     if (type >= 0 && type < resultTypes.length)
